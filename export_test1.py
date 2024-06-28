c1abe4fab720dcdc62e4d00b5bfbc2bc81cb7f1b
jedi007 <360751194@qq.com>
2024-06-28 15:17:55 +0800

    export auto batch

--- export_test1.py
+++ export_test1.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
+import torch
 
-model = YOLO("yolov8n.pt")
-model.half()
-# model.export(format="onnx", dynamic=True)
-model.export(format="onnx", device=0)
+model = YOLO("yolov8m.pt")
+# model.half()
+# model.export(format="onnx", dynamic=True, batch=-1)
+model.export(format="onnx", device=0, dynamic=True, simplify=True)
