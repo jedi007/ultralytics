d1af58154e599ee4554237ba0991b7a1948ff4c3
jedi007 <360751194@qq.com>
2024-11-18 16:10:16 +0800

    add val py

---
+++ det_val.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("helmet_241009.pt")
+
+# Train the model
+# train_results = model.train(
+#     data="safety.yaml",  # path to dataset YAML
+#     epochs=0,  # number of training epochs
+#     imgsz=(384, 640),  # training image size
+#     device=0,  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
+# )
+
+# Evaluate model performance on the validation set
+# metrics = model.val()
+model.val(data="safety.yaml", imgsz=(384, 640))
